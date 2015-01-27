6d409c6f98de5102814da25a11da676440460c6e
jack <522185660@qq.com>
2015-01-27 21:31:10 +0800

    first commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareButton
+//
+//  Created by lbencs on 10/22/14.
+//  Copyright (c) 2014 lbencs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
